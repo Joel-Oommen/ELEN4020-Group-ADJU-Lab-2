a7f0c32004faa23bf1ce602e2084f132deae8a21
Ashraf-Omar <ashrafomar6@gmail.com>
2018-03-03 10:23:16 +0200

    Created transpose function

--- main.c
+++ main.c
@@ -3,33 +3,55 @@
 
 void swap_int(int *x, int *y) {int tmp = *x;*x=*y;*y=tmp;}
 
-#define ELEM 4
+void transpose(int Dimensions,int A[Dimensions][Dimensions],int rowStart,int colStart,int quadSize)
+{
+	for(int n=0;n<quadSize;n++)
+	{
+		for(int m=n+1;m<quadSize;m++)
+		{
+			swap_int(&A[rowStart+n][colStart+m],&A[rowStart+m][colStart+n]);
+		}
+	}
+}
+
+#define Dimensions 4
 
 int main()
 {
-	int A[ELEM][ELEM];
+	int A[Dimensions][Dimensions];
 	int c=0;
 	printf("\n Original matrix \n");
-	for(int i=0;i<ELEM;i++)
+	for(int i=0;i<Dimensions;i++)
 	{
-		for(int j=0;j<ELEM;j++)
+		for(int j=0;j<Dimensions;j++)
 		{
 			A[i][j]=++c;
 			printf("%d \t", A[i][j]);
 		}
 		printf("\n");
 	}
-	for(int n=0;n<ELEM;n++)
-	{
-		for(int m=n+1;m<ELEM;m++)
-		{
-			swap_int(&A[n][m],&A[m][n]);
-		}
-	}
+	/*This diagram only works on tab width 4
+	_____________________
+	|		  |			|
+	|	A	  |	B		|
+	|		  |			|
+	|_________|_________|
+	|		  |			|
+	|	C	  |	D		|
+	|		  |			|
+	|		  |			|
+	*********************
+	*/
+	int Q = Dimensions/2;
+	transpose(Dimensions,A,0,0,Q);//A
+	transpose(Dimensions,A,0,Q,Q);//B
+	transpose(Dimensions,A,Q,0,Q);//C
+	transpose(Dimensions,A,Q,Q,Q);//D
+	//swap B and C
 	printf("\n Transpose matrix \n");
-	for(int i=0;i<ELEM;i++)
+	for(int i=0;i<Dimensions;i++)
 	{
-		for(int j=0;j<ELEM;j++)
+		for(int j=0;j<Dimensions;j++)
 		{
 			printf("%d \t", A[i][j]);
 		}
