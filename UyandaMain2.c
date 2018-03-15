8aff10384c86fa2f8c48529f203514798d33945a
1147279 <1147279@students.wits.ac.za>
2018-03-15 21:07:55 +0200

    Uyanda with timing and transpose check

--- UyandaMain2.c
+++ UyandaMain2.c
@@ -5,6 +5,8 @@
 #include <math.h>
 #include <stdbool.h>
 #include <sys/time.h>
+#include "omp.h"
+#include <time.h>
 struct information
 {
 	int lowerBound;
@@ -16,29 +18,65 @@ struct information
 void* transposeMatrix(void* thread);
 int upperBound(int arrSize, int BinNumber, int NumberOfDivisions);
 int lowerBound(int arrSize, int BinNumber, int NumberOfDivisions);
+
+bool checkTranspose(int *A,int *B, int dimension)
+{
+	bool equal = true;
+	int ELEM = dimension;
+	for(int i=0;i<ELEM;i++)
+	{
+		for(int j=0;j<ELEM;j++)
+		{
+			if (A[i*ELEM+j]!=B[i*ELEM+j])
+			{
+				equal=false;
+			}
+		}
+	}
+	return equal;
+}
+
 int main() 
 {
-	int numOfThreads = 4;
-	int dimension = 32;
+	int numOfThreads =128;
+	int dimension =8192;
 	int arraySize = dimension*dimension;
-	int Arr[arraySize];
+	int * Arr =malloc(arraySize*sizeof(int));
+	int * B =malloc(arraySize*sizeof(int));
 	int division = arraySize/numOfThreads;
 	pthread_t thread[numOfThreads];
 	struct information threadArgument[numOfThreads];
 	
-	for(int i = 0; i < arraySize; i++)
-		Arr[i] = i + 1;
+	for(int i = 0; i < dimension; i++)
+	{
+		for (int j = 0; j < dimension; j++)
+		{
+			B[j*dimension+i] = i*dimension+j;		
+			Arr[i*dimension+j] = i*dimension+j;
+		}
+	}
 	
-	
+	/*
+	for(int i = 0; i < dimension; i++)
+	{
+		for(int j = 0; j < dimension; j++)
+		{
+			printf("%d \t ",Arr[i*dimension + j]);
+			
+		}printf("\n");
+	}
+	printf("\n");
 	
 	for(int i = 0; i < dimension; i++)
 	{
 		for(int j = 0; j < dimension; j++)
 		{
-			printf("%d \t",Arr[i*dimension + j]);
+			printf("%d \t ",B[i*dimension + j]);
+			
 		}printf("\n");
 	}
 	printf("\n");
+	*/
 	printf("\n");
 	
 	
@@ -50,17 +88,35 @@ int main()
 		threadArgument[i].Arr = Arr;
 		
 	}
-	
+
+
+	double starttime = omp_get_wtime();
+
 	for(int i = 0; i < numOfThreads; i++)
 	{
 		pthread_create (&thread[i], NULL, &transposeMatrix,&threadArgument[i]);	
 	}
-	
+
+
 	for(int i = 0; i < numOfThreads; i++)
 	{
 		pthread_join (thread[i], NULL);	
 	}
 	
+	double diff = omp_get_wtime() - starttime;
+
+	printf("Pthread: %f \n", diff);
+
+
+	bool x;
+
+	//Check transpose took place correctly. 
+
+	x=checkTranspose(Arr,B,dimension);
+
+	printf("Matrix transposed correctly: %s \n", x?"true":"false");
+	
+	/*
 	
 	for(int i = 0; i < dimension; i++)
 	{
@@ -70,6 +126,8 @@ int main()
 		}printf("\n");
 	}
 	printf("\n");
+
+	*/
 	printf("\n");
 	
 	
